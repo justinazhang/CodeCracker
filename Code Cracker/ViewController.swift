0e9512915f01bf16d57de3c1875d340ba42ded49
Justin Zhang <zhangjustin77@gmail.com>
2020-06-27 22:15:47 -0700

    Initial Commit

---
+++ Code Cracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Cracker
+//
+//  Created by Justin Zhang on 6/27/20.
+//  Copyright © 2020 Justin Zhang Justin Kaufman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
